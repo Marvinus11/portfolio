fcdd1055ca1753187a91acfc7860bf54a6f65e2e
Marvinus11 <viniciusmartins880@yahoo.com.br>
2024-04-25 02:47:49 -0300

    Create design mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     server: {
         port: 3000,
         host: true
-    }
+    },
+    integrations: [icon()],
 });
